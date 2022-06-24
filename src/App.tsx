1532dd54d5ca45abe7343082a443a2897f3a29b8
Hossein Davoodi <h.davodi2002@gmail.com>
2022-06-24 11:35:14 +0300

    some functions are added to handle the clicks to animate card

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,10 @@ const App = () => {
   const [back, setBack] = useState(0);
   const [backtext, setBacktext] = useState(0)
   
-
+  const descriptionClick =(id: number) => {
+    setBack(id)
+    setBacktext(id)
+  }
 
 
 useEffect(()=>{
@@ -34,15 +37,15 @@ useEffect(()=>{
     {
       data.map((val)=>(
           <div key={val.id} className="flip reletive inset-x-0 mx-auto mt-5 text-white ">
-    <div className='card w-full h-full'>
+    <div className={back === val.id? 'card w-full h-full is-flipped' : 'card w-full h-full'}>
       {/* card front */}
       <div className="absolute rounded-2xl pt-5 pl-8 pr-5 w-full h-full card__face front ">
         <h2>{val.text} <br /> {val.textT}</h2>
-        <div className="open absolute"></div>
+        <div className="open absolute" onClick={() => descriptionClick(val.id)}></div>
       </div>
       {/* card back */}
       <div className="absolute rounded-2xl pt-5 pl-8 pr-5 w-full h-full card__face back">
-        <div className='backtextb'>
+        <div className={backtext === val.id? 'backtext' : 'backtextb'}>
         <h2 className="mb-5">About {val.text}</h2>
         <p>{val.description} </p>
         </div>
